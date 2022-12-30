ed087faa8b3b38778b869cf5a5a772453c4066f0
DESKTOP-EINE1B2\주현석 <joohyunseok0313@gmail.com>
2022-12-30 16:47:00 +0900

    add : airflow 소스 추가 - 작업중

---
+++ airflow/streamify_dag.py
@@ -0,0 +1,84 @@
+import os
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+
+
+EVENTS = ['listen_events', 'page_view_events', 'auth_events'] 
+
+
+GCP_PROJECT_ID = {your_project_id}
+GCP_GCS_BUCKET = {your_bucket_id}
+BIGQUERY_DATASET = os.environ.get('BIGQUERY_DATASET', 'streamify_stg')
+
+EXECUTION_MONTH = '{{ logical_date.strftime("%-m") }}'
+EXECUTION_DAY = '{{ logical_date.strftime("%-d") }}'
+EXECUTION_HOUR = '{{ logical_date.strftime("%-H") }}'
+EXECUTION_DATETIME_STR = '{{ logical_date.strftime("%m%d%H") }}'
+
+TABLE_MAP = { f"{event.upper()}_TABLE" : event for event in EVENTS}
+
+MACRO_VARS = {"GCP_PROJECT_ID":GCP_PROJECT_ID, 
+              "BIGQUERY_DATASET": BIGQUERY_DATASET, 
+              "EXECUTION_DATETIME_STR": EXECUTION_DATETIME_STR
+              }
+
+MACRO_VARS.update(TABLE_MAP)
+
+default_args = {
+    'owner' : 'airflow'
+}
+
+with DAG(
+    dag_id = f'streamify_dag',
+    default_args = default_args,
+    description = f'Hourly data pipeline to generate dims and facts for streamify',
+    schedule_interval="5 * * * *", # 매 시간 : 5분 마다 dag실행
+    start_date=datetime(2022,12,30,18),
+    catchup=False,
+    max_active_runs=1,
+    user_defined_macros=MACRO_VARS,
+    tags=['streamify']
+) as dag:
+    
+
+
+    for event in EVENTS:
+        
+        staging_table_name = event
+        insert_query = f"{{% include 'sql/{event}.sql' %}}" #extra {} for f-strings escape
+        external_table_name = f'{staging_table_name}_{EXECUTION_DATETIME_STR}'
+        events_data_path = f'{staging_table_name}/month={EXECUTION_MONTH}/day={EXECUTION_DAY}/hour={EXECUTION_HOUR}'
+        events_schema = schema[event]
+
+        create_external_table_task = create_external_table(event,
+                                                           GCP_PROJECT_ID, 
+                                                           BIGQUERY_DATASET, 
+                                                           external_table_name, 
+                                                           GCP_GCS_BUCKET, 
+                                                           events_data_path)
+
+        create_empty_table_task = create_empty_table(event,
+                                                     GCP_PROJECT_ID,
+                                                     BIGQUERY_DATASET,
+                                                     staging_table_name,
+                                                     events_schema)
+                                                
+        execute_insert_query_task = insert_job(event,
+                                               insert_query,
+                                               BIGQUERY_DATASET,
+                                               GCP_PROJECT_ID)
+
+        delete_external_table_task = delete_external_table(event,
+                                                           GCP_PROJECT_ID, 
+                                                           BIGQUERY_DATASET, 
+                                                           external_table_name)
+                    
+        
+        create_external_table_task >> \
+        create_empty_table_task >> \
+        execute_insert_query_task >> \
+        delete_external_table_task >> \
+        initate_dbt_task >> \
+        execute_dbt_task
